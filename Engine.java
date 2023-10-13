e7483550b33b09ee75e181055693ef13b79b9065
Mattea29 <98430244+Mattea29@users.noreply.github.com>
2023-10-13 13:45:54 -0400

    adding public methods and trying to figure out how to set FuelType

--- Engine.java
+++ Engine.java
@@ -3,6 +3,12 @@ public class Engine {
     double currentFuelLevel;
     double maxFuelLevel;
 
+    public Engine(double currentFuelLevel, double maxFuelLevel) {
+        this.f = FuelType;
+        this.currentFuelLevel = currentFuelLevel;
+        this.maxFuelLevel = maxFuelLevel;
+    }
+
     public double getCurrentFuelLevel() {
         return this.currentFuelLevel;
     }
@@ -11,12 +17,27 @@ public class Engine {
         return this.maxFuelLevel;
     }
 
-    public void setCurrentFuelLevel(double currentFuelLevel) {
-        this.currentFuelLevel = currentFuelLevel;
-    }
+    //public void setCurrentFuelLevel(double currentFuelLevel) {
+        //this.currentFuelLevel = currentFuelLevel;
+    //}
     
-    public void setMaxFuelLevel(double maxFuelLevel) {
-        this.maxFuelLevel = maxFuelLevel;
+    //public void setMaxFuelLevel(double maxFuelLevel) {
+        //this.maxFuelLevel = maxFuelLevel;
+    //}
+
+    public void refuel(double currentFuelLevel) {
+        this.currentFuelLevel = this.maxFuelLevel;
+    }
+
+    public void go(double currentFuelLevel) {
+        if (this.currentFuelLevel > 0) {
+            System.out.println("Current fuel level is " + currentFuelLevel);
+            currentFuelLevel = currentFuelLevel - 1;
+            System.out.println("Remaining fuel level is " + currentFuelLevel);
+
+        } else {
+            throw new RuntimeException("Fuel Level Too Low to Go.");
+        }
     }
 
 
