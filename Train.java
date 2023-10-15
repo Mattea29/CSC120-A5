af8d4a3547535bd413d83ccfcfeeb17f266c0995
Mattea29 <98430244+Mattea29@users.noreply.github.com>
2023-10-15 15:04:36 -0400

    updating train class

--- Train.java
+++ Train.java
@@ -1,10 +1,27 @@
-import java.util.ArrayList
+import java.util.ArrayList;
 
 public class Train {
     private final Engine engine;
     private ArrayList<Car> cars;
 
-    public Train(FuelType, fuelType, double fuelCapacity, int nCars, int passengerCapacity) {
-        
+    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity) {
+        this.engine = new Engine(fuelType, fuelCapacity);
+        this.cars = new ArrayList<>(); 
     }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Car getCar(int i) {
+        if (i >= 0 && i < cars.size()) {
+            return cars.get(i);
+        }
+        return null;
+    }
+
+    public int getMaxCapacity() {
+        int MaxCapacity = 0;
+    }
+
 }
