732918da6e2ff466de140a1a90fee910451ebdec
Mattea29 <98430244+Mattea29@users.noreply.github.com>
2023-10-15 15:04:57 -0400

    forgot to commit other changes to other classes

--- Car.java
+++ Car.java
@@ -6,13 +6,63 @@ public class Car {
 
 
     // Constructor
+    public Car(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        this.passengersOnboard = new ArrayList<Passenger>(maxCapacity);
+    }
 
     public int getCapacity() {
-        // the maximum number of people onboard
+        return this.maxCapacity;
     }
+
 
     public int seatsRemaining() {
-        // maximum number - the total number of people currently onboard
+        //maximum number - the total number of people currently onboard
+        return(this.maxCapacity - this.passengersOnboard.size());
     }
 
+    public void addPassenger(Passenger p) {
+        try {
+        if (seatsRemaining() != 0) {
+            System.out.println("Seats remaining: " + seatsRemaining());
+            System.out.println("Adding passenger " + p);
+            passengersOnboard.add(p);
+            System.out.println("Current passengers on board: " + passengersOnboard);
+        } 
+        } catch(Exception e) {
+            System.err.println("All seats taken");
+
+        }
+    }
+
+    public void removePassenger(Passenger p) {
+        try {
+        if (passengersOnboard.contains(p)) {
+            System.out.println("Removing passenger from train");
+            passengersOnboard.remove(p);
+            System.out.println("Passengers on board: " + passengersOnboard);
+        }
+        } catch(Exception e) {
+            System.err.println("Passenger not on board and cannot be removed");
+        }
+    }
+
+    public void printManifest() {
+        try{
+            if (passengersOnboard.size() != 0) {
+                System.out.println(passengersOnboard);
+            }
+        } catch (Exception e) {
+            System.out.println("This car is empty");
+        }
+    }
+
+    //public static void main(String[] args) {
+        //Car myCar = new Car(20);
+        //myCar.getCapacity();
+        //myCar.seatsRemaining();
+       // myCar.addPassenger("Jessica");
+
+
+//}
 }
--- Passenger.java
+++ Passenger.java
@@ -5,4 +5,18 @@ public class Passenger {
     public Passenger(String name) {
         this.name = name;
     }
+
+    public void boardCar(Car c) {
+        try {
+            c.addPassenger(this);
+        } catch(Exception e) {
+            System.err.println("Cannot add this passenger");
+        }
+    }
+
+    
+   public void getOffCar(Car c) {
+    c.removePassenger(this);
+         
+}
 }
